34b1d66826af6a1162d41a189386e58f9711c982
Agustín Guzmán <agustin.guzman.v@proton.me>
2026-03-31 21:32:15 -0300

    format

--- clock/segment.c
+++ clock/segment.c
@@ -1,38 +1,33 @@
 #include "segment.h"
 #include <math.h>
 
-extern const int WIDTH;
-extern const int HEIGHT;
-
-Segment new_segment(Vector2 center, float angle) {
-  int length = HEIGHT * 0.4f;
-  int thickness = WIDTH * 0.01f;
-
-  return (Segment){center, angle, length, thickness};
+Segment new_segment(Vector2 position, float angle) {
+	return (Segment){position, angle, SEG_LENGTH, SEG_THICKNESS};
 }
 
 void draw_segment(Segment *segment) {
-  float x = segment->position.x;
-  float y = segment->position.y;
-  int l = segment->length;
-  int t = segment->thickness;
+	float x = segment->position.x;
+	float y = segment->position.y;
+	int l = segment->length;
+	int t = segment->thickness;
 
-  float cosA = cosf(-segment->angle);
-  float sinA = sinf(-segment->angle);
+	float cosA = cosf(-segment->angle);
+	float sinA = sinf(-segment->angle);
 
-  Vector2 dir = (Vector2){cosA, sinA};
-  Vector2 perp = (Vector2){-sinA, cosA};
+	Vector2 dir = (Vector2){cosA, sinA};
+	Vector2 perp = (Vector2){-sinA, cosA};
 
-  char len = 6;
-  Vector2 points[] = {{x - dir.x * (l + t), y - dir.y * (l + t)},
+	char len = 6;
+	Vector2 points[] = {
+		{x - dir.x * (l + t), y - dir.y * (l + t)},
 
-                      {x - dir.x * l + perp.x * t, y - dir.y * l + perp.y * t},
-                      {x - dir.x * l - perp.x * t, y - dir.y * l - perp.y * t},
+		{x - dir.x * l + perp.x * t, y - dir.y * l + perp.y * t},
+		{x - dir.x * l - perp.x * t, y - dir.y * l - perp.y * t},
 
-                      {x + dir.x * l + perp.x * t, y + dir.y * l + perp.y * t},
-                      {x + dir.x * l - perp.x * t, y + dir.y * l - perp.y * t},
+		{x + dir.x * l + perp.x * t, y + dir.y * l + perp.y * t},
+		{x + dir.x * l - perp.x * t, y + dir.y * l - perp.y * t},
 
-                      {x + dir.x * (l + t), y + dir.y * (l + t)}};
+		{x + dir.x * (l + t), y + dir.y * (l + t)}};
 
-  DrawTriangleStrip(points, len, ON);
+	DrawTriangleStrip(points, len, ON);
 }
--- clock/segment.h
+++ clock/segment.h
@@ -4,13 +4,19 @@
 #include <raylib.h>
 
 typedef struct Segment {
-  Vector2 position;
-  float angle;
-  int length;
-  int thickness;
+	Vector2 position;
+	float angle;
+	int length;
+	int thickness;
 } Segment;
 
 #define ON RED
+
+extern const int WIDTH;
+extern const int HEIGHT;
+
+#define SEG_LENGTH HEIGHT * 0.15f
+#define SEG_THICKNESS WIDTH * 0.01f
 
 Segment new_segment(Vector2 position, float angle);
 void draw_segment(Segment *segment);
