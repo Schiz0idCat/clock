8a0e243ff4f4262ca61020592d9ca6cd25723f03
Agustín Guzmán <agustin.guzman.v@proton.me>
2026-03-31 21:32:32 -0300

    format and draw a number

--- main.c
+++ main.c
@@ -4,27 +4,29 @@ const int WIDTH = 1200;
 const int HEIGHT = 400;
 
 int main() {
-  const time_t curr_time = time(NULL);
-  char *time_str = ctime(&curr_time);
+	const time_t curr_time = time(NULL);
+	char *time_str = ctime(&curr_time);
 
-  InitWindow(WIDTH, HEIGHT, TITLE);
+	InitWindow(WIDTH, HEIGHT, TITLE);
 
-  Segment segment = new_segment(CENTER, deg2rad(90));
+	Digit digit = new_digit(CENTER);
+	// Segment segment = new_segment(CENTER, deg2rad(180));
 
-  while (!WindowShouldClose()) {
-    if (IsKeyPressed(KEY_Q)) {
-      break;
-    }
+	while (!WindowShouldClose()) {
+		if (IsKeyPressed(KEY_Q)) {
+			break;
+		}
 
-    BeginDrawing();
-    ClearBackground(BLACK);
+		BeginDrawing();
+		ClearBackground(BLACK);
 
-    draw_segment(&segment);
+		draw_digit(&digit);
+		// draw_segment(&segment);
 
-    EndDrawing();
-  }
+		EndDrawing();
+	}
 
-  CloseWindow();
+	CloseWindow();
 
-  return 0;
+	return 0;
 }
