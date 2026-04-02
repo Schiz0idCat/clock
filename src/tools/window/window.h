d7d69c73bbde99921515982990d8119addd4d8ea
Agustín Guzmán <agustin.guzman.v@proton.me>
2026-04-02 14:00:31 -0300

    resizable center

--- src/tools/window/window.h
+++ src/tools/window/window.h
@@ -5,11 +5,9 @@
 
 #define TITLE "Digital Clock"
 
-extern const int WIDTH;
-extern const int HEIGHT;
-
-#define CENTER (Vector2){WIDTH / 2.0f, HEIGHT / 2.0f}
-#define CENTER_X WIDTH / 2.0f
-#define CENTER_Y HEIGHT / 2.0f
+#define CENTER_X GetScreenWidth() / 2.0f
+#define CENTER_Y GetScreenHeight() / 2.0f
+#define CENTER                                                                 \
+	(Vector2) { CENTER_X, CENTER_Y }
 
 #endif
