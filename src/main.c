2372a86135bda9c1c361b4dc9b7299bf4f805a17
Agustín Guzmán <agustin.guzman.v@proton.me>
2026-04-07 18:54:05 -0400

    fix: embedded windows size
    for some reason, GetCurrentMonitor() doesn't work

--- src/main.c
+++ src/main.c
@@ -1,19 +1,12 @@
 #include "clock.h"
 
 int main() {
-	int monitor = GetCurrentMonitor();
-
-	int screen_width = GetMonitorWidth(monitor);
-	int screen_height = GetMonitorHeight(monitor);
-
-	int window_width = (int)(screen_width * 0.7f);
-	int window_height = (int)(screen_height * 0.4f);
+	int window_width = 800;
+	int window_height = 450;
 
 	SetConfigFlags(FLAG_WINDOW_RESIZABLE);
 	SetTargetFPS(30);
 	InitWindow(window_width, window_height, TITLE);
-
-	SetWindowPosition(screen_width / 2, screen_height / 2);
 
 	Time t = new_time();
 
